ce681c8ccff8e8e350189c1b0f80ab5aebc372be
otabekabduhalilov2010 <otabekabduhalilov80@gmail.com>
2024-12-20 16:48:16 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Hero />
+    </>
+  )
+}
+
+export default App
