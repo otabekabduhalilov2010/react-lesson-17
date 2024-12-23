5f13c2dcb0679653e926069565cd3a08d9b5a456
otabekabduhalilov2010 <otabekabduhalilov80@gmail.com>
2024-12-23 17:02:12 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Section1 from './components/Section1/Section1'
+import Section2 from './components/Section2/Section2'
+import Tarifs from './components/Tarifs/Tarifs'
+import Used from './components/Used/Used'
+import Level from './components/Level/Level'
+
+
 
 const App = () => {
   return (
     <>
     <Header />
     <Hero />
+    <Section1 />
+    <Section2 />
+    <Tarifs />
+    <Used />
+    <Level />
     </>
   )
 }
